df10c9de11ebc7dbe15170fb7dc3146fadcf68c9
Adam Wier <wier.adam@gmail.com>
2024-10-19 12:59:45 +0200

    Adapt for update and syndiction

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const core = require("@actions/core");
 const Micropub = require("micropub-helper");
-const parsePost = require("./parse-post");
+// const parsePost = require("./parse-post");
 
 async function run() {
   try {
     const token = core.getInput("token");
-    const postInput = core.getInput("post");
     const endpoint = core.getInput("endpoint");
+    const feedUrl = core.getInput("feedUrl");
     const micropub = new Micropub({
       token,
       clientId: "https://github.com/grantcodes/micropub-action",
       micropubEndpoint: endpoint,
     });
-    const { post, type } = parsePost(postInput);
 
-    console.log(`Creating ${type} encoded micropub post on ${endpoint}`);
-    console.log(post);
+    info(`Fetching ${feedUrl} …`);
+    const feedContent = await fetch(feedUrl).then((response) => response.json());
+    let items = feedContent.items;
+    let latest = items[0];
+    let tootUrl = latest.toots[0];
+    let originalUrl = latest.id;
 
-    const url = await micropub.create(post, type);
+
+    console.log(`Updating ${originalUrl} with link ${tootUrl}`);
+
+    const url = await micropub.update(originalUrl, {
+      add: {
+        syndication: tootUrl
+      }
+    });
 
     console.log(`Successfully posted to ${url}`);
     core.setOutput("url", url);
